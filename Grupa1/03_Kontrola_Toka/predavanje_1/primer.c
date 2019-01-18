7a416a1efd2769cede7d4e6aa061f4428dd503f4
Vahid Zekic <vahidzekic@gmail.com>
2019-01-18 09:00:00 +0100

    feat: Grupa 1 — Kontrola toka — predavanje 1

---
+++ Grupa1/03_Kontrola_Toka/predavanje_1/primer.c
@@ -0,0 +1,20 @@
+/// fajl: Grupa1/03_Kontrola_Toka/predavanje_1/primer.c
+#include <stdio.h>
+
+int main(void) {
+    int broj;
+
+    printf("Unesite ceo broj: ");
+    if (scanf("%d", &broj) != 1) {
+        printf("Greska pri unosu!\n");
+        return 1;
+    }
+
+    if (broj % 2 == 0) {
+        printf("Broj %d je PARAN.\n", broj);
+    } else {
+        printf("Broj %d je NEPARAN.\n", broj);
+    }
+
+    return 0;
+}
