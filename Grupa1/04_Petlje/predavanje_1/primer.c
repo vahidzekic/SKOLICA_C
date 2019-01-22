b6977116784446679408e3751829dba4230a419e
Vahid Zekic <vahidzekic@gmail.com>
2019-01-22 09:00:00 +0100

    feat: Grupa 1 — Petlje — predavanje 1

---
+++ Grupa1/04_Petlje/predavanje_1/primer.c
@@ -0,0 +1,16 @@
+/// fajl: Grupa1/04_Petlje/predavanje_1/primer.c
+#include <stdio.h>
+
+int main(void) {
+    int i = 1;
+    int suma = 0;
+
+    // Sabiranje brojeva od 1 do 10
+    while (i <= 10) {
+        suma += i;
+        i++;
+    }
+
+    printf("Suma brojeva od 1 do 10 je: %d\n", suma);
+    return 0;
+}
