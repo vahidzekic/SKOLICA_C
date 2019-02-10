8c941f7fbdc0244c48fa040f2e49471380ce090d
Vahid Zekic <vahidzekic@gmail.com>
2019-02-10 09:00:00 +0100

    feat: Grupa 1 — Stringovi — predavanje 1

---
+++ Grupa1/08_Stringovi/predavanje_1/lekcija.md
@@ -0,0 +1,4 @@
+# Predavanje 1: Stringovi i Učitavanje sa fgets
+
+## Teorijsko Objašnjenje
+String u C-u je običan niz karaktera koji se završava null-karakterom (`'\0'`). Za bezbedan unos stringova sa tastature koristi se funkcija `fgets` umesto zastarele `gets`.
---
+++ Grupa1/08_Stringovi/predavanje_1/primer.c
@@ -0,0 +1,14 @@
+/// fajl: Grupa1/08_Stringovi/predavanje_1/primer.c
+#include <stdio.h>
+
+int main(void) {
+    char ime[30];
+
+    printf("Unesite vase ime: ");
+    // fgets(niz, max_duzina, ulazni_tok)
+    if (fgets(ime, sizeof(ime), stdin) != NULL) {
+        printf("Zdravo, %s", ime); // fgets ucitava i znak za novi red '\n'
+    }
+
+    return 0;
+}
