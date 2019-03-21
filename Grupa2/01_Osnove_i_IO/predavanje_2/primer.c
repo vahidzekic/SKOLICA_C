ccc75b8beb9264bd0ddc5a29950c571cfc62d047
Vahid Zekic <vahidzekic@gmail.com>
2019-03-21 09:00:00 +0100

    feat: Grupa 2 — Osnove i I/O — predavanje 2

---
+++ Grupa2/01_Osnove_i_IO/predavanje_2/primer.c
@@ -0,0 +1,17 @@
+/// fajl: Grupa2/01_Osnove_i_IO/predavanje_2/primer.c
+#include <stdio.h>
+
+int main(void) {
+    int godine;
+    float visina;
+
+    printf("Unesite vase godine: ");
+    scanf("%d", &godine);
+
+    printf("Unesite vasu visinu (u metrima): ");
+    scanf("%f", &visina);
+
+    printf("Imate %d godina i visoki ste %.2f metara.\n", godine, visina);
+
+    return 0;
+}
