ab665bc3c8f7a2413194760fb3d03b86cb47218c
Vahid Zekic <vahidzekic@gmail.com>
2019-04-09 09:00:00 +0100

    feat: Grupa 2 — Nizovi i matrice — predavanje 2

---
+++ Grupa2/05_Nizovi_i_Matrice/predavanje_2/primer.c
@@ -0,0 +1,30 @@
+/// fajl: Grupa2/05_Nizovi_i_Matrice/predavanje_2/primer.c
+#include <stdio.h>
+
+#define RED 2
+#define KOL 3
+
+int main(void) {
+    int matrica[RED][KOL] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+    int transponovana[KOL][RED];
+
+    // Transponovanje matrice
+    for (int i = 0; i < RED; i++) {
+        for (int j = 0; j < KOL; j++) {
+            transponovana[j][i] = matrica[i][j];
+        }
+    }
+
+    printf("Transponovana matrica:\n");
+    for (int i = 0; i < KOL; i++) {
+        for (int j = 0; j < RED; j++) {
+            printf("%d ", transponovana[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
