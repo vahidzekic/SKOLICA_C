fef36caa2623a5a34d0a8d42ad8187077fba9ce6
Vahid Zekic <vahidzekic@gmail.com>
2019-04-20 09:00:00 +0100

    feat: Grupa 2 — Pokazivači (pointeri) — predavanje 3

---
+++ Grupa2/07_Pokazivaci_Pointers/predavanje_3/primer.c
@@ -0,0 +1,17 @@
+/// fajl: Grupa2/07_Pokazivaci_Pointers/predavanje_3/primer.c
+#include <stdio.h>
+
+// Funkcija prima adrese dve promenljive
+void zameni(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(void) {
+    int x = 5, y = 10;
+    printf("Pre zamene: x = %d, y = %d\n", x, y);
+    zameni(&x, &y);
+    printf("Posle zamene: x = %d, y = %d\n", x, y);
+    return 0;
+}
