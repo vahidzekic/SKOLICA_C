40b4ae17da29f493db7a2e2b2bb64a5cd312a8a5
Vahid Zekic <vahidzekic@gmail.com>
2019-04-28 09:00:00 +0100

    feat: Grupa 2 — Strukture (structs) — predavanje 2

---
+++ Grupa2/09_Strukture_Structs/predavanje_2/lekcija.md
@@ -0,0 +1,4 @@
+# Predavanje 2: Nizovi Struktura
+
+## Teorijsko Objašnjenje
+Možemo kreirati nizove čiji su elementi strukture. Ovo se koristi za modelovanje jednostavnih baza podataka u memoriji.
---
+++ Grupa2/09_Strukture_Structs/predavanje_2/primer.c
@@ -0,0 +1,22 @@
+/// fajl: Grupa2/09_Strukture_Structs/predavanje_2/primer.c
+#include <stdio.h>
+
+typedef struct {
+    char ime[30];
+    double prosek;
+} Student;
+
+int main(void) {
+    Student studenti[3] = {
+        {"Vahid", 9.5},
+        {"Marko", 8.2},
+        {"Jelena", 9.8}
+    };
+
+    printf("Spisak studenata:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("%s (Prosek: %.2f)\n", studenti[i].ime, studenti[i].prosek);
+    }
+
+    return 0;
+}
