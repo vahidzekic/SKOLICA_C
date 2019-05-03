0ca689a1889282d7b5043c7e75cbcefd6b2c2375
Vahid Zekic <vahidzekic@gmail.com>
2019-05-03 09:00:00 +0100

    feat: Grupa 2 — Rad sa fajlovima — predavanje 2

---
+++ Grupa2/10_Rad_Sa_Fajlovima/predavanje_2/primer.c
@@ -0,0 +1,17 @@
+/// fajl: Grupa2/10_Rad_Sa_Fajlovima/predavanje_2/primer.c
+#include <stdio.h>
+
+int main(void) {
+    FILE *f = fopen("studenti.txt", "w");
+    if (f == NULL) {
+        return 1;
+    }
+
+    // Pisanje formatiranih podataka
+    fprintf(f, "Ime: %s | Prosek: %.2f\n", "Vahid", 9.85);
+    fputs("Kraj baze podataka.\n", f);
+
+    fclose(f);
+    printf("Podaci su uspesno upisani.\n");
+    return 0;
+}
