cb27a8056bf2cf5085fa4575190b4a2e0fa3f56f
Vahid Zekic <vahidzekic@gmail.com>
2019-05-04 09:00:00 +0100

    feat: Grupa 2 — Rad sa fajlovima — predavanje 3

---
+++ Grupa2/10_Rad_Sa_Fajlovima/predavanje_3/primer.c
@@ -0,0 +1,20 @@
+/// fajl: Grupa2/10_Rad_Sa_Fajlovima/predavanje_3/primer.c
+#include <stdio.h>
+
+int main(void) {
+    char linija[100];
+    FILE *f = fopen("studenti.txt", "r");
+
+    if (f == NULL) {
+        printf("Fajl ne postoji ili se ne moze otvoriti!\n");
+        return 1;
+    }
+
+    printf("Sadrzaj fajla:\n");
+    while (fgets(linija, sizeof(linija), f) != NULL) {
+        printf("%s", linija);
+    }
+
+    fclose(f);
+    return 0;
+}
