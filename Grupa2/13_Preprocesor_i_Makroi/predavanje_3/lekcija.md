9fb5e0f6884ca09de97afab683d77308a459dd76
Vahid Zekic <vahidzekic@gmail.com>
2019-05-18 09:00:00 +0100

    feat: Grupa 2 — Preprocesor i makroi — predavanje 3

---
+++ Grupa2/13_Preprocesor_i_Makroi/predavanje_3/lekcija.md
@@ -0,0 +1,4 @@
+# Predavanje 3: Uslovno Kompajliranje
+
+## Teorijsko Objašnjenje
+Uslovno kompajliranje omogućava da se delovi koda uključe ili isključe iz kompajliranja na osnovu definisanih makroa (`#ifdef`, `#ifndef`, `#else`, `#endif`).
---
+++ Grupa2/13_Preprocesor_i_Makroi/predavanje_3/primer.c
@@ -0,0 +1,14 @@
+/// fajl: Grupa2/13_Preprocesor_i_Makroi/predavanje_3/primer.c
+#include <stdio.h>
+
+#define DEBUG_MOD
+
+int main(void) {
+#ifdef DEBUG_MOD
+    printf("[DEBUG] Program je pokrenut u debug modu.\n");
+#endif
+
+    printf("Glavni program izvrsava zadatak.\n");
+
+    return 0;
+}
