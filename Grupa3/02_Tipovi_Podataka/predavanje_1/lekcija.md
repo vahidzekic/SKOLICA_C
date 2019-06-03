619d51f7e8e549372ddff69be9d2bc725050a0d2
Vahid Zekic <vahidzekic@gmail.com>
2019-06-03 09:00:00 +0100

    feat: Grupa 3 — Tipovi podataka — predavanje 1

---
+++ Grupa3/02_Tipovi_Podataka/predavanje_1/lekcija.md
@@ -0,0 +1,4 @@
+# Predavanje 1: Celobrojni Tipovi i Veličina Memorije
+
+## Teorijsko Objašnjenje
+Celobrojni tipovi podataka u C-u su `char`, `short`, `int` i `long`. Razlikuju se po opsegu vrednosti koje mogu da sačuvaju i veličini memorije koju zauzimaju (u bajtovima), što proveravamo operatorom `sizeof`.
---
+++ Grupa3/02_Tipovi_Podataka/predavanje_1/primer.c
@@ -0,0 +1,11 @@
+/// fajl: Grupa3/02_Tipovi_Podataka/predavanje_1/primer.c
+#include <stdio.h>
+#include <limits.h>
+
+int main(void) {
+    printf("Velicina char: %lu bajt (Opseg: %d do %d)\n", sizeof(char), CHAR_MIN, CHAR_MAX);
+    printf("Velicina short: %lu bajta (Opseg: %d do %d)\n", sizeof(short), SHRT_MIN, SHRT_MAX);
+    printf("Velicina int: %lu bajta (Opseg: %d do %d)\n", sizeof(int), INT_MIN, INT_MAX);
+    printf("Velicina long: %lu bajtova (Opseg: %ld do %ld)\n", sizeof(long), LONG_MIN, LONG_MAX);
+    return 0;
+}
