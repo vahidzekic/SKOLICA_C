5200c5afc50ac28b0b882425558d23fef034d9c1
Vahid Zekic <vahidzekic@gmail.com>
2019-06-26 09:00:00 +0100

    feat: Grupa 3 — Pokazivači (pointeri) — predavanje 1

---
+++ Grupa3/07_Pokazivaci_Pointers/predavanje_1/primer.c
@@ -0,0 +1,17 @@
+/// fajl: Grupa3/07_Pokazivaci_Pointers/predavanje_1/primer.c
+#include <stdio.h>
+
+int main(void) {
+    int x = 42;
+    int *p = &x; // p dobija adresu od x
+
+    printf("Vrednost x: %d\n", x);
+    printf("Adresa x (&x): %p\n", (void*)&x);
+    printf("Vrednost p (adresa): %p\n", (void*)p);
+    printf("Dereferencirano p (*p): %d\n", *p);
+
+    *p = 100; // Menjamo vrednost x preko pokazivaca
+    printf("Nova vrednost x: %d\n", x);
+
+    return 0;
+}
