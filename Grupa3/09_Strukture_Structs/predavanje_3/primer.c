55e50d1a54394253c128bcf8e4ca5378417d931b
Vahid Zekic <vahidzekic@gmail.com>
2019-07-09 09:00:00 +0100

    feat: Grupa 3 — Strukture (structs) — predavanje 3

---
+++ Grupa3/09_Strukture_Structs/predavanje_3/primer.c
@@ -0,0 +1,20 @@
+/// fajl: Grupa3/09_Strukture_Structs/predavanje_3/primer.c
+#include <stdio.h>
+
+typedef struct {
+    int x;
+    int y;
+} Tacka;
+
+void pomeri(Tacka *t, int dx, int dy) {
+    t->x += dx;
+    t->y += dy;
+}
+
+int main(void) {
+    Tacka t1 = {10, 20};
+    printf("Pre pomeranja: (%d, %d)\n", t1.x, t1.y);
+    pomeri(&t1, 5, -10);
+    printf("Posle pomeranja: (%d, %d)\n", t1.x, t1.y);
+    return 0;
+}
