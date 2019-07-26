38da6e73cb8be9de0fbc2e298c5c9483f6bec6a2
Vahid Zekic <vahidzekic@gmail.com>
2019-07-26 09:00:00 +0100

    feat: Grupa 3 — Preprocesor i makroi — predavanje 2

---
+++ Grupa3/13_Preprocesor_i_Makroi/predavanje_2/primer.c
@@ -0,0 +1,15 @@
+/// fajl: Grupa3/13_Preprocesor_i_Makroi/predavanje_2/primer.c
+#include <stdio.h>
+
+// Pravilno zagradjivanje da bi se sacuvao prioritet operacija
+#define KVADRAT(x) ((x) * (x))
+#define MAKS(a, b) (((a) > (b)) ? (a) : (b))
+
+int main(void) {
+    int n = 3;
+    printf("Kvadrat od %d je %d\n", n, KVADRAT(n));
+    // Bez zagrada bi KVADRAT(n + 1) bio n + 1 * n + 1 = 3 + 3 + 1 = 7, a sa zagradama je (3+1)*(3+1) = 16
+    printf("Kvadrat od %d+1 je %d\n", n, KVADRAT(n + 1));
+    printf("Maksimum od 10 i 20 je %d\n", MAKS(10, 20));
+    return 0;
+}
