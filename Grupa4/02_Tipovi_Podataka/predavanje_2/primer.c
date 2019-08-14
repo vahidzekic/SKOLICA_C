d092adaa3a15bfddb034d8b2dae9ac91c76f3fdd
Vahid Zekic <vahidzekic@gmail.com>
2019-08-14 09:00:00 +0100

    feat: Grupa 4 — Tipovi podataka — predavanje 2

---
+++ Grupa4/02_Tipovi_Podataka/predavanje_2/primer.c
@@ -0,0 +1,12 @@
+/// fajl: Grupa4/02_Tipovi_Podataka/predavanje_2/primer.c
+#include <stdio.h>
+#include <float.h>
+
+int main(void) {
+    float f = 3.1415926535f;
+    double d = 3.141592653589793;
+
+    printf("Float (ispisan sa 10 decimala): %.10f (Zauzima %lu bajta)\n", f, sizeof(f));
+    printf("Double (ispisan sa 15 decimala): %.15f (Zauzima %lu bajta)\n", d, sizeof(d));
+    return 0;
+}
