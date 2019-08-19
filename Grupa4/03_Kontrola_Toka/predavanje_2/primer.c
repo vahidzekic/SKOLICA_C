12814a0ce8e0cf3029d55f069f8cf4bed3727d56
Vahid Zekic <vahidzekic@gmail.com>
2019-08-19 09:00:00 +0100

    feat: Grupa 4 — Kontrola toka — predavanje 2

---
+++ Grupa4/03_Kontrola_Toka/predavanje_2/primer.c
@@ -0,0 +1,29 @@
+/// fajl: Grupa4/03_Kontrola_Toka/predavanje_2/primer.c
+#include <stdio.h>
+
+int main(void) {
+    int poeni;
+
+    printf("Unesite broj poena (0-100): ");
+    scanf("%d", &poeni);
+
+    if (poeni < 0 || poeni > 100) {
+        printf("Nevazeci poeni!\n");
+    } else {
+        if (poeni >= 90) {
+            printf("Ocena: 10\n");
+        } else if (poeni >= 80) {
+            printf("Ocena: 9\n");
+        } else if (poeni >= 70) {
+            printf("Ocena: 8\n");
+        } else if (poeni >= 60) {
+            printf("Ocena: 7\n");
+        } else if (poeni >= 51) {
+            printf("Ocena: 6\n");
+        } else {
+            printf("Niste polozili (Ocena: 5)\n");
+        }
+    }
+
+    return 0;
+}
