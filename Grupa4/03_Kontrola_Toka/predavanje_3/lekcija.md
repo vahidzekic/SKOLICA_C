60ede93a6446084edf6b28aaa1aa1bc175d5653c
Vahid Zekic <vahidzekic@gmail.com>
2019-08-20 09:00:00 +0100

    feat: Grupa 4 — Kontrola toka — predavanje 3

---
+++ Grupa4/03_Kontrola_Toka/predavanje_3/lekcija.md
@@ -0,0 +1,4 @@
+# Predavanje 3: Višestruki Izbor uz switch Naredbu
+
+## Teorijsko Objašnjenje
+Naredba `switch` je efikasnija alternativa za `else-if` lance kada se vrednost jedne celobrojne promenljive ili karaktera upoređuje sa više konstantnih vrednosti (`case`). Ključna reč `break` prekida dalje izvršavanje.
---
+++ Grupa4/03_Kontrola_Toka/predavanje_3/primer.c
@@ -0,0 +1,22 @@
+/// fajl: Grupa4/03_Kontrola_Toka/predavanje_3/primer.c
+#include <stdio.h>
+
+int main(void) {
+    int dan;
+
+    printf("Unesite redni broj dana u nedelji (1-7): ");
+    scanf("%d", &dan);
+
+    switch (dan) {
+        case 1: printf("Ponedeljak\n"); break;
+        case 2: printf("Utorak\n"); break;
+        case 3: printf("Sreda\n"); break;
+        case 4: printf("Cetvrtak\n"); break;
+        case 5: printf("Petak\n"); break;
+        case 6: printf("Subota\n"); break;
+        case 7: printf("Nedelja\n"); break;
+        default: printf("Nepostojeci dan! Unesite broj od 1 do 7.\n"); break;
+    }
+
+    return 0;
+}
