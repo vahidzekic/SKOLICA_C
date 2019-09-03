7de5c7da4be656ea1d5c8ec0e0790fd0560ea633
Vahid Zekic <vahidzekic@gmail.com>
2019-09-03 09:00:00 +0100

    feat: Grupa 4 — Funkcije — predavanje 3

---
+++ Grupa4/06_Funkcije/predavanje_3/primer.c
@@ -0,0 +1,17 @@
+/// fajl: Grupa4/06_Funkcije/predavanje_3/primer.c
+#include <stdio.h>
+
+int faktorijel(int n) {
+    // Bazni slucaj
+    if (n <= 1) {
+        return 1;
+    }
+    // Rekurzivni korak
+    return n * faktorijel(n - 1);
+}
+
+int main(void) {
+    int broj = 5;
+    printf("Faktorijel broja %d je %d\n", broj, faktorijel(broj));
+    return 0;
+}
