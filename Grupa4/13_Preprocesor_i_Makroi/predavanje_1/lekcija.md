54b121ef63a4bcc721a36ea0ee3757b27c4e69a9
Vahid Zekic <vahidzekic@gmail.com>
2019-10-03 09:00:00 +0100

    feat: Grupa 4 — Preprocesor i makroi — predavanje 1

---
+++ Grupa4/13_Preprocesor_i_Makroi/predavanje_1/lekcija.md
@@ -0,0 +1,4 @@
+# Predavanje 1: Preprocesorske Direktive i Konstante
+
+## Teorijsko Objašnjenje
+Preprocesor radi pre samog kompajliranja koda. Direktiva `#define` se koristi za definisanje konstanti koje se u fazi preprocesiranja zamenjuju vrednošću.
---
+++ Grupa4/13_Preprocesor_i_Makroi/predavanje_1/primer.c
@@ -0,0 +1,15 @@
+/// fajl: Grupa4/13_Preprocesor_i_Makroi/predavanje_1/primer.c
+#include <stdio.h>
+
+#define PI 3.14159
+#define MAKS_STUDENATA 100
+
+int main(void) {
+    double r = 5.0;
+    double povrsina = r * r * PI;
+
+    printf("Povrsina kruga: %.2f\n", povrsina);
+    printf("Maksimalan broj studenata: %d\n", MAKS_STUDENATA);
+
+    return 0;
+}
