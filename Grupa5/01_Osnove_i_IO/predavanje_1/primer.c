c474bcc7aac66ad5d643e39a4a25d800f7363cf8
Vahid Zekic <vahidzekic@gmail.com>
2019-10-17 09:00:00 +0100

    feat: Grupa 5 — Osnove i I/O — predavanje 1

---
+++ Grupa5/01_Osnove_i_IO/predavanje_1/primer.c
@@ -0,0 +1,8 @@
+/// fajl: Grupa5/01_Osnove_i_IO/predavanje_1/primer.c
+#include <stdio.h>
+
+int main(void) {
+    // Jednostavan ispis poruke na ekranu
+    printf("Zdravo, svete! Dobrodosli u SKOLICU C (Grupa 5).\n");
+    return 0;
+}
