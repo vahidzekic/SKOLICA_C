19935cbb3e6c24a08aa668eb69d3fa62fcdbdfa1
Vahid Zekic <vahidzekic@gmail.com>
2019-10-20 09:00:00 +0100

    feat: Grupa 5 — Osnove i I/O — predavanje 3

---
+++ Grupa5/01_Osnove_i_IO/predavanje_3/primer.c
@@ -0,0 +1,18 @@
+/// fajl: Grupa5/01_Osnove_i_IO/predavanje_3/primer.c
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    double a = 5.0;
+    double b = 3.0;
+
+    double zbir = a + b;
+    double proizvod = a * b;
+    double stepen = pow(a, b); // a na stepen b
+
+    printf("Zbir: %.2f\n", zbir);
+    printf("Proizvod: %.2f\n", proizvod);
+    printf("Stepenovanje (%.1f^%.1f): %.2f\n", a, b, stepen);
+
+    return 0;
+}
