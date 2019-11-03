7eecddb4c48c377d6b9a98ee0bbc8548047d8d10
Vahid Zekic <vahidzekic@gmail.com>
2019-11-03 09:00:00 +0100

    feat: Grupa 5 — Petlje — predavanje 3

---
+++ Grupa5/04_Petlje/predavanje_3/primer.c
@@ -0,0 +1,23 @@
+/// fajl: Grupa5/04_Petlje/predavanje_3/primer.c
+#include <stdio.h>
+
+int main(void) {
+    int broj;
+    int suma = 0;
+
+    printf("Unosite cele brojeve (unos 0 prekida program):\n");
+    do {
+        printf("Unos: ");
+        scanf("%d", &broj);
+
+        if (broj < 0) {
+            printf("Preskacemo negativne brojeve.\n");
+            continue; // Preskače sabiranje
+        }
+
+        suma += broj;
+    } while (broj != 0);
+
+    printf("Suma pozitivnih unetih brojeva je: %d\n", suma);
+    return 0;
+}
