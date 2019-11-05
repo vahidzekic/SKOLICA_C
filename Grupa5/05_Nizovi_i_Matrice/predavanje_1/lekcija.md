b8aa0b38fa1c5ae6dbe66bff4c92d205c3c014f0
Vahid Zekic <vahidzekic@gmail.com>
2019-11-05 09:00:00 +0100

    feat: Grupa 5 — Nizovi i matrice — predavanje 1

---
+++ Grupa5/05_Nizovi_i_Matrice/predavanje_1/lekcija.md
@@ -0,0 +1,4 @@
+# Predavanje 1: Jednodimenzionalni Nizovi
+
+## Teorijsko Objašnjenje
+Niz je skup elemenata istog tipa smeštenih na uzastopnim memorijskim lokacijama. Pristup elementima se vrši preko indeksa (koji počinju od 0).
---
+++ Grupa5/05_Nizovi_i_Matrice/predavanje_1/primer.c
@@ -0,0 +1,16 @@
+/// fajl: Grupa5/05_Nizovi_i_Matrice/predavanje_1/primer.c
+#include <stdio.h>
+
+int main(void) {
+    int ocene[5] = {9, 8, 10, 7, 9};
+    int suma = 0;
+    double prosek;
+
+    for (int i = 0; i < 5; i++) {
+        suma += ocene[i];
+    }
+
+    prosek = (double)suma / 5;
+    printf("Prosecna ocena iznosi: %.2f\n", prosek);
+    return 0;
+}
