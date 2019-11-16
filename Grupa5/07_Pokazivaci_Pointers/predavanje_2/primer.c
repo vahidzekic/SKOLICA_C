6e9a1fa29788dc7dc8372a322f218e8a459d81a2
Vahid Zekic <vahidzekic@gmail.com>
2019-11-16 09:00:00 +0100

    feat: Grupa 5 — Pokazivači (pointeri) — predavanje 2

---
+++ Grupa5/07_Pokazivaci_Pointers/predavanje_2/primer.c
@@ -0,0 +1,13 @@
+/// fajl: Grupa5/07_Pokazivaci_Pointers/predavanje_2/primer.c
+#include <stdio.h>
+
+int main(void) {
+    int niz[3] = {10, 20, 30};
+    int *ptr = niz; // Istovetno sa ptr = &niz[0]
+
+    for (int i = 0; i < 3; i++) {
+        printf("Element %d: %d (Adresa: %p)\n", i, *(ptr + i), (void*)(ptr + i));
+    }
+
+    return 0;
+}
