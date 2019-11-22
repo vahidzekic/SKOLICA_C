2e665233d6e678247d0169504d08c40688189cd5
Vahid Zekic <vahidzekic@gmail.com>
2019-11-22 09:00:00 +0100

    feat: Grupa 5 — Stringovi — predavanje 3

---
+++ Grupa5/08_Stringovi/predavanje_3/primer.c
@@ -0,0 +1,20 @@
+/// fajl: Grupa5/08_Stringovi/predavanje_3/primer.c
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void) {
+    char tekst[] = "Programiranje C 2026";
+    int i = 0;
+
+    printf("Original: %s\n", tekst);
+
+    while (tekst[i] != '\0') {
+        if (islower((unsigned char)tekst[i])) {
+            tekst[i] = (char)toupper((unsigned char)tekst[i]);
+        }
+        i++;
+    }
+
+    printf("Velikim slovima: %s\n", tekst);
+    return 0;
+}
