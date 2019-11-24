085d1f05ac863845caeb617ef12563b494d60c4b
Vahid Zekic <vahidzekic@gmail.com>
2019-11-24 09:00:00 +0100

    feat: Grupa 5 — Strukture (structs) — predavanje 1

---
+++ Grupa5/09_Strukture_Structs/predavanje_1/primer.c
@@ -0,0 +1,18 @@
+/// fajl: Grupa5/09_Strukture_Structs/predavanje_1/primer.c
+#include <stdio.h>
+
+typedef struct {
+    char naslov[50];
+    char autor[50];
+    int godina;
+} Knjiga;
+
+int main(void) {
+    Knjiga k1 = {"Na Drini cuprija", "Ivo Andric", 1945};
+
+    printf("Naslov: %s\n", k1.naslov);
+    printf("Autor: %s\n", k1.autor);
+    printf("Godina izdanja: %d\n", k1.godina);
+
+    return 0;
+}
