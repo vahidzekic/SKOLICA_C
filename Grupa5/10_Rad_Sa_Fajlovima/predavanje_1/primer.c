40c69851c2078d254539924e28c756bd623b8f8e
Vahid Zekic <vahidzekic@gmail.com>
2019-11-28 09:00:00 +0100

    feat: Grupa 5 — Rad sa fajlovima — predavanje 1

---
+++ Grupa5/10_Rad_Sa_Fajlovima/predavanje_1/primer.c
@@ -0,0 +1,16 @@
+/// fajl: Grupa5/10_Rad_Sa_Fajlovima/predavanje_1/primer.c
+#include <stdio.h>
+
+int main(void) {
+    FILE *f = fopen("test.txt", "w");
+
+    if (f == NULL) {
+        printf("Greska pri kreiranju fajla!\n");
+        return 1;
+    }
+
+    printf("Fajl je uspesno kreiran.\n");
+    fclose(f);
+
+    return 0;
+}
