8e36a7670611eebc11b6043560615d506dd3d442
Vahid Zekic <vahidzekic@gmail.com>
2019-12-09 09:00:00 +0100

    feat: Grupa 5 — Bitovne operacije — predavanje 2

---
+++ Grupa5/12_Bitovne_Operacije/predavanje_2/primer.c
@@ -0,0 +1,12 @@
+/// fajl: Grupa5/12_Bitovne_Operacije/predavanje_2/primer.c
+#include <stdio.h>
+
+int main(void) {
+    unsigned int x = 5; // 0000 0101
+
+    printf("x << 1 = %u (Mnozenje sa 2)\n", x << 1); // 10 (0000 1010)
+    printf("x << 2 = %u (Mnozenje sa 4)\n", x << 2); // 20 (0001 0100)
+    printf("x >> 1 = %u (Deljenje sa 2)\n", x >> 1); // 2  (0000 0010)
+
+    return 0;
+}
